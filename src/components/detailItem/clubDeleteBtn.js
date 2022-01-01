39643e49d169faef76e5b8b14ac44d9d345fe726
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-01-01 18:19:40 +0900

    [#9] create club delete confirm modal

--- src/components/detailItem/clubDeleteBtn.js
+++ src/components/detailItem/clubDeleteBtn.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, useHistory } from "react-router-dom"
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 import { useDispatch } from 'react-redux';
 import { deleteClub } from "../../actions/index";
@@ -8,6 +9,7 @@ import { deleteClub } from "../../actions/index";
 import "./detailItem.css"
 
 const ClubDeleteBtn = () => {
+    const [deleteConfirm, setDeleteConfirm] = useState(false);
     const { id } = useParams();
     const history = useHistory();
     const dispatch = useDispatch();
@@ -17,14 +19,38 @@ const ClubDeleteBtn = () => {
         history.replace("/");
     }
 
+    const closeDeleteConfirm = () => setDeleteConfirm(false);
+    const showDeleteConfirm = () => setDeleteConfirm(true);
+
+    const confirmButton = {
+        width: "100%",
+        borderRadius: "8px 8px 0px 0px",
+        fontSize: "14px"
+    }
+
     return (
         <div className="club-delete-btn">
             <Button 
                 variant="outline-secondary"
                 size="sm"
                 className="delete-button"
-                onClick={clickedDeleteBtn}
+                onClick={showDeleteConfirm}
             >삭제</Button>
+            <Modal dialogClassName='custom-dialog' show={deleteConfirm} onHide={closeDeleteConfirm}>
+                <Modal.Body className="modal-text">정말로 삭제하시겠습니까?</Modal.Body>
+                <div style={{width: "100%"}}>
+                    <div class="left">
+                        <Button style={confirmButton} variant="secondary" onClick={closeDeleteConfirm}>
+                            취소
+                        </Button>
+                    </div>
+                    <div class="right">
+                        <Button style={confirmButton} variant="primary" onClick={clickedDeleteBtn}>
+                            삭제
+                        </Button>
+                    </div>
+                </div>
+            </Modal>
         </div>
     )
 }
