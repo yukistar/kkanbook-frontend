f06282bee74f28df68cb674cc36312a3740d1508
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-01-06 16:40:57 +0900

    [#11] edit style for club detail page

--- src/components/clubDetail/clubBook.js
+++ src/components/clubDetail/clubBook.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import kakaoKey from "../../properties/kakaoKey";
 import "./clubDetail.css"
 
 const ClubBook = (props) => {
+    const [author, setAuthor] = useState("");
+    const [datetime, setDatetime] = useState("");
+    const [publisher, setPublisher] = useState("");
+    const [url, setUrl] = useState("");
+
+    const kakao = (keyword) => {
+        return axios.create({
+            baseURL: "https://dapi.kakao.com/v3/search/book?target=title",
+            headers: { Authorization: kakaoKey },
+            params: { query: keyword }
+        });
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const kakaoRes = await kakao(props.bookTitle).get();
+            setAuthor(kakaoRes.data.documents[0].authors[0]);
+            setDatetime(kakaoRes.data.documents[0].datetime.substr(0, 10));
+            setPublisher(kakaoRes.data.documents[0].publisher);
+            setUrl(kakaoRes.data.documents[0].url);
+        };
+        fetchData();
+    }, [props.bookTitle]);
+
     return (
         <div className="club-book">
-            <img 
-                className="detail-book-image" 
-                src={props.bookImage} 
-                alt="book"
-            />
+            <div className="club-book-box">
+                <img 
+                    className="detail-book-image" 
+                    src={props.bookImage} 
+                    alt="book"
+                />
+                <div style={{fontWeight: "600"}}>{props.bookTitle}</div>
+                <div>{author} 저 | {publisher} | {datetime}</div>
+                <div className="book-link">
+                    이 책에 대해 더 자세히 알고 싶다면,<br/> <a href={url}>여기</a>를 클릭해주세요.
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/clubDetail/clubDetail.js
+++ src/components/clubDetail/clubDetail.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { FaBook, FaCalendarAlt } from 'react-icons/fa';
 
+import CountDown from "../detailItem/countDown";
+import ClubChatBtn from "../detailItem/clubChatBtn";
 import ClubBook from "./clubBook";
 import "./clubDetail.css"
 
@@ -10,18 +12,36 @@ const ClubDetail = () => {
     const clubs = useSelector(state => state.clubs);
     const { id } = useParams();
 
+    let now = new Date(); // 현재시간
+    let clubStartStr = clubs[id].clubTime.substring(0, 10) + " " + clubs[id].clubTime.substring(15, 20) + ":00";
+    let clubStartTime = new Date(clubStartStr);
+
+    let interval = clubStartTime - now;
+
+    let days = parseInt(interval / (24 * 60 * 60 * 1000));
+    let hours = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((interval % (1000 * 60)) / 1000);
+
+    const hoursMinSecs = {hours: (days * 24 + hours), minutes: minutes, seconds: seconds};
+
     return (
-        <div className="col">
-            <div className="club-detail row">
+        <div>
+            <ClubBook 
+                bookTitle={clubs[id].bookTitle} bookImage={clubs[id].bookImage}
+            />
+            <div className="club-detail">
                 <div className="detail-club-title">{clubs[id].clubTitle}</div>
                 <div className="detail-club-description">
                     {clubs[id].clubDescription}
                 </div>
                 <div className="detail-book-title"><FaBook /> {clubs[id].bookTitle}</div>
                 <div className="detail-club-time"><FaCalendarAlt /> {clubs[id].clubTime}</div>
+                {interval <= 0 ?
+                    <ClubChatBtn />
+                    : <CountDown hoursMinSecs={hoursMinSecs}/>
+                }
             </div>
-
-            <ClubBook bookImage={clubs[id].bookImage}/>
         </div>
     )
 }
