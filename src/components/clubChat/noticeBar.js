3a3cfd54f739648b7cec0db3049d93e423abd641
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-02-03 16:13:11 +0900

    [#19] Edit noticeBar style

--- src/components/clubChat/noticeBar.js
+++ src/components/clubChat/noticeBar.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import './clubChat.css'
 import Button from "react-bootstrap/Button";
 import styled from "@emotion/styled";
+import { HiSpeakerphone } from 'react-icons/hi';
 
 const NoticeInput = styled.textarea`
     background-color: transparent;
     border: none;
-    width: ${props => (props.noticeBtn ? "calc(100% - 50px)" : "100%")};
-    height: 40px;
-    overflow-y: hidden;
+    width: ${props => (props.noticeBtn ? "calc(100% - 50px)" : "99%")};
+    resize: vertical;
+    min-height: 40px;
+    max-height: 500px;
     ::placeholder {
         color: black;
         text-align: center;
@@ -23,18 +25,23 @@ const NoticeBar = (props) => {
 
   return (
     <div className="notice-bar">
-        <NoticeInput
-            value={props.notice} noticeBtn={noticeBtn} placeholder="공지사항을 등록해보세요!"
-            onChange={(event) => {props.setNotice(event.target.value); showNoticeBtn();}}
-        />
+        <div style={{float: "left"}}>
+            <HiSpeakerphone size="33" style={{position: "relative", top:"6px"}} />
+        </div>
+        <div style={{width: "calc(100% - 40px)" , float: "right"}}>
+            <NoticeInput contentEditable
+                value={props.notice} noticeBtn={noticeBtn} placeholder="공지사항을 등록해보세요!"
+                onChange={(event) => {props.setNotice(event.target.value); showNoticeBtn();}}
+            />
         {noticeBtn?
             <Button 
                 variant="outline-secondary" size="sm" className="basic-button"
                 onClick={(event) => {props.sendNotice(event); closeNoticeBtn();}}
-                style={{position: "absolute", top:"8px"}}
+                style={{position: "absolute", top:"6px"}}
             >등록</Button>   
         :null
         }
+        </div>
     </div>
   )
 }
